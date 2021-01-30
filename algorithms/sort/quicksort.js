c43e5b57bcb4bcc62b3aeabee6dec6682f6e0cf6
poilkjmn22 <sghi688886ihg@163.com>
2021-01-30 22:34:31 +0800

    add quicksort.js

---
+++ algorithms/sort/quicksort.js
@@ -0,0 +1,32 @@
+import { swap,isArray } from './util.js';
+
+function partition(arr, left, right) {
+  let pivot = (left + right) >> 1; //中枢值这里取中位数
+  let store = left;
+	swap(arr, pivot, right); //中枢值交换到末尾
+	for(let i = left; i < right; i++) {
+		if(arr[i] <= arr[right]) {
+			swap(arr, i, store);
+			store += 1;
+		}
+	}
+	swap(arr, store, right);
+	return store;
+}
+
+function quicksort(arr, left, right) {
+	if(left < right) {
+		let pivot = partition(arr, left, right);
+		quicksort(arr, 0, pivot - 1);
+		quicksort(arr, pivot + 1, right);
+	}
+}
+
+export function sort(arr) {
+  if (!isArray(arr)) {
+    console.warn(`${arr} is not an array`);
+    return arr;
+  }
+	quicksort(arr, 0, arr.length - 1);
+	return arr;
+}
--- algorithms/test.js
+++ algorithms/test.js
@@ -1,5 +1,6 @@
 import { sort as sortInsert } from './sort/insert.js';
 import { sort as sortHeapify } from './sort/heapify.js';
+import { sort as sortQuick } from './sort/quicksort.js';
 
 console.log(sortInsert([5, 4, 3, 2, 1]));
 console.log(sortInsert([15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14]));
@@ -10,3 +11,8 @@ console.log(sortInsert('g'));
 console.log(sortHeapify([8, 11, 9, 2, 10, 16]));
 console.log(sortHeapify([15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14]));
 console.log(sortHeapify(10));
+
+
+console.log(sortQuick([15, 9, 8, 1, 4, 11, 7, 12, 13, 6, 5, 3, 16, 2, 10, 14]));
+console.log(sortQuick([7,6,5,4,3,2,1]));
+console.log(sortQuick('quicksort'));
