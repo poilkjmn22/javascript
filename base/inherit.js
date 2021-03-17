3549e80d4164f17638196b6b76867848d00f8caa
poilkjmn22 <sghi688886ihg@163.com>
2021-03-18 00:57:04 +0800

    update:./base/inherit.js:行为委托的设计模式

--- base/inherit.js
+++ base/inherit.js
@@ -4,24 +4,24 @@ const object = (o) => {
   return new F();
 };
 
-export default function inheritPrototype(subType, superType){
-  let prototype = object(superType);
-  prototype.constructor = subType;
-  subType.prototype = prototype;
+export default function inheritPrototype(SubType, SuperType){
+  let prototype = object(SuperType);
+  prototype.constructor = SubType;
+  SubType.prototype = prototype;
 }
 
 /*****
   使用行为委托的设计模式实现类继承《你不知道的Javascript（上）-5～6章》
   对象关联是一种编码风格：它倡导直接创建和关联对象，不把他们抽象成类.
 *****/
-var superType = {
+var SuperType = {
   name: 'fangqi',
   sayName(){
     console.log(`My name is ${this.name}`);
   }
 };
 
-var subType = Object.create(superType, {
+var SubType = Object.create(SuperType, {
   age: {
     value: 31
   },
@@ -33,7 +33,9 @@ var subType = Object.create(superType, {
   }
 });
 
-var subType2 = Object.create(subType, {
+var subType1 = Object.create(SubType);
+
+var subType2 = Object.create(SubType, {
   age: {
     value: 32
   },
@@ -42,11 +44,13 @@ var subType2 = Object.create(subType, {
   }
 });
 
-subType.sayAge();
-console.log(Object.getPrototypeOf(subType) === superType);
-subType.name = 'fangqi3';
-subType.sayAge();
+subType1.sayAge();
+console.log(Object.getPrototypeOf(subType1) === SubType);
+subType1.name = 'fangqi3';
+subType1.sayAge();
+
+var superType = Object.create(SuperType);
 superType.sayName();
 
 subType2.sayAge();
-console.log(Object.getPrototypeOf(subType2) === subType);
+console.log(Object.getPrototypeOf(subType2) === SubType);
